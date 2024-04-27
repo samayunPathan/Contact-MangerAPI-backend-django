8bc4360de3aed672ee4427b3bce7e97468566e47
samayun <samayunp@gmail.com>
2024-04-27 23:43:12 +0600

    create user model

--- contact_manager/contactAPI/models.py
+++ contact_manager/contactAPI/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+    )
+
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    def create_user(self,email,password=None):
+        if not email:
+            raise ValueError('User must have email !')
+        email=self.normalize_email(email)
+        user=self.model(email=email)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self,email,password):
+        user=self.create_user(email,password)
+        user.is_superuser=True
+        user.is_staff=True
+        user.save(using=self._db)
+
+        return user
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    email=models.EmailField(max_length=255,unique=True)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=True)
+
+    objects=UserProfileManager()
+
+    USERNAME_FIELD="email"
+
+    def __str__(self):
+        return self.email
+
--- contact_manager/contact_manager/settings.py
+++ contact_manager/contact_manager/settings.py
@@ -75,6 +75,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL="contactAPI.UserProfile"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
